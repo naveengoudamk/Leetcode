d857d0c2ff056b910698a16baf915deea33d3372
Naveengouda M K <navi112004naveen@gmail.com>
2026-08-23 12:09:22 +0530

    Time: 8 ms (5.64%), Space: 50.2 MB (5.28%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -2,7 +2,7 @@ class Solution {
     public String mergeAlternately(String word1, String word2) {
         if(word1.isEmpty()) return word2;
         if(word2.isEmpty()) return word1;
-        else
+        
 
         return "" + word1.charAt(0) + word2.charAt(0) + 
         mergeAlternately(word1.substring(1) , word2.substring(1));
