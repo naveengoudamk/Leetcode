7c0ff9d14b302f9ea240f75034cc3a5c06e880ce
Naveengouda M K <navi112004naveen@gmail.com>
2026-09-20 08:53:39 +0530

    Time: 1 ms (100%), Space: 44.3 MB (10.65%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int totalDegree = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            int reversedAlphabetPos = 26 - (s.charAt(i) - 'a');
+            int stringPos = i + 1;
+            totalDegree += reversedAlphabetPos * stringPos;
+        }
+        
+        return totalDegree;
+    }
+}
